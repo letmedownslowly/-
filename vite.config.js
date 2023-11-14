0c6bf08a9e87eae54502607384bbad4f645bf1e6
aria <2101922097@qq.com>
2023-11-14 18:43:08 +0800

    scss文件的自动导入

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     Components({
       resolvers: [
         // 1.配置elementPlus采用sass样式配色系统
-        ElementPlusResolver({ importStyle:'sass' })
+        ElementPlusResolver({ importStyle: 'sass' })
       ],
     }),
   ],
@@ -34,6 +34,7 @@ export default defineConfig({
         // 2.自动导入定制化样式文件进行样式覆盖
         additionalData: `
           @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
         `,
       }
     }
