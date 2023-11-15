07ca1de3ebbe06ca6ef32d436a8b6c450c84873f
aria <2101922097@qq.com>
2023-11-15 17:06:15 +0800

    home-图片懒加载指令实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,14 +1,20 @@
 import httpInstance from '@/utils/http'
 
 // 封装获取banner接口
-export function getBannerAPI(){
+export function getBannerAPI() {
     return httpInstance({
-        url:'/home/banner'
+        url: '/home/banner'
     })
 }
 
-export function findNewAPI(){
+export function findNewAPI() {
     return httpInstance({
-        url:'/home/new'
+        url: '/home/new'
+    })
+}
+
+export function findHotAPI() {
+    return httpInstance({
+        url: '/home/hot'
     })
 }
--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 // 测试接口函数
 // import {getCategory} from '@/apis/testAPI'
 // getCategory().then(res=>{
@@ -18,3 +20,22 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        // el代表指令绑定的元素
+        // binding一个对象,包含如binding.value（指令等于后面绑定的值）等属性
+        // console.log(el, binding);
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                // console.log(isIntersecting);
+                if(isIntersecting){
+                    // 代表图片进入视口区
+                    el.src = binding.value
+                }
+            },
+        )
+    }
+})
