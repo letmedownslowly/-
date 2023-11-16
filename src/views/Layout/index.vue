56c4ea50da6d835a1370f7c0c83fdfd08c5cde8d
aria <2101922097@qq.com>
2023-11-16 11:48:39 +0800

    路由缓存问题解决

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -9,13 +9,15 @@ import { useCategoryStore } from "@/stores/category";
 import { onMounted } from "vue";
 const categoryStore = useCategoryStore();
 
-onMounted(()=>categoryStore.getCategory())
+onMounted(() => categoryStore.getCategory());
 </script>
 
 <template>
   <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
+  <!-- 方案一：添加key 破坏复用机制，强制销毁重建 -->
+  <!-- <RouterView :key="$route.fullPath" /> -->
   <RouterView />
   <LayoutFooter />
 </template>
