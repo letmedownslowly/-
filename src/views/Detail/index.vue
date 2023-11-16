d5e0368f10c7af12f104097238a8e2e2d49119b9
aria <2101922097@qq.com>
2023-11-16 22:23:56 +0800

    详情热榜-基础组件封装

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import { getDetail } from "@/apis/detail";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
+import DetailHot from "./components/DetailHot.vue"
 
 const route = useRoute();
 const goods = ref({});
@@ -119,7 +120,12 @@ onMounted(() => getGoods());
               </div>
             </div>
             <!-- 24热榜+专题推荐 -->
-            <div class="goods-aside"></div>
+            <div class="goods-aside">
+              <!-- 24小时 -->
+              <DetailHot></DetailHot>
+              <!-- 周 -->
+              <DetailHot></DetailHot>
+            </div>
           </div>
         </div>
       </div>
