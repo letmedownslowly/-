79865e3bf6a29a47f1c623779f1ee0e482f6b973
aria <2101922097@qq.com>
2023-11-17 17:47:01 +0800

    登录-使用token适配多模板显示

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,4 +1,6 @@
 <script setup>
+import {useUserStore} from '@/stores/user'
+const userStore = useUserStore()
 </script>
 
 <template>
@@ -6,9 +8,10 @@
     <div class="container">
       <ul>
         <!-- 多模板渲染，区分登录状态和非登录状态 -->
-        <template v-if="false">
+        <!-- 适配思路：登陆时显示第一块 非登陆时显示第二块 是否有token -->
+        <template v-if="userStore.userInfo.token">
           <li>
-            <a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a>
+            <a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a>
           </li>
           <li>
             <el-popconfirm
