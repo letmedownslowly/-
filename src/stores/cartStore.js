b48fb3247ff7d9d26ba8b605e04d814fb640ea8a
aria <2101922097@qq.com>
2023-11-17 19:45:33 +0800

    本地-头部购物车-删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,28 +1,36 @@
 import { defineStore } from "pinia";
-import {ref} from 'vue'
-export const useCartStore = defineStore('cart',()=>{
+import { ref } from 'vue'
+export const useCartStore = defineStore('cart', () => {
     // 1.定义state - cartList
     const cartList = ref([])
     // 2.定义action - addCart
-    const addCart = (goods)=>{
+    const addCart = (goods) => {
         // 添加购物车操作
         // 已添加过 - count +1
         // 没添加过 - push
         // 思路：通过匹过来的skuId能不能在cartList中找到，找到了就是添加过
-        const item = cartList.value.find((item)=> goods.skuId === item.skuId)
-        if(item){
+        const item = cartList.value.find((item) => goods.skuId === item.skuId)
+        if (item) {
             // 找到
             item.count++
         }
-        else{
+        else {
             // 没找到
             cartList.value.push(goods)
         }
     }
+    const delCart = (skuId) => {
+        // 思路：
+        // 1. 找到要删除项的下标值 - splice
+        // 2. 使用数组的过滤方法 - filter
+        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+        cartList.value.splice(idx, 1)
+    }
     return {
         cartList,
-        addCart
+        addCart,
+        delCart
     }
-},{
+}, {
     persist: true,
 })
