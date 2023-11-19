e229fd0f2fb553f0040eb06fc898c3b1a38c8fb0
aria <2101922097@qq.com>
2023-11-19 11:37:56 +0800

    结算页-提交订单业务实现

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,6 +1,10 @@
 <script setup>
-import { getCheckInfoAPI } from "@/apis/checkout";
+import { getCheckInfoAPI, createOrderAPI } from "@/apis/checkout";
+import {useRouter} from "vue-router";
 import { onMounted, ref } from "vue";
+import {useCartStore} from '@/stores/cartStore'
+const router = useRouter()
+const cartStore = useCartStore()
 const checkInfo = ref({}); // 订单对象
 const curAddress = ref({});
 const getCheckInfo = async () => {
@@ -26,7 +30,33 @@ const switchAddress = (item) => {
 const confirm = () => {
   curAddress.value = activeAddress.value;
   showDialog.value = false;
-  activeAddress.value = {}
+  activeAddress.value = {};
+};
+
+// 创建订单
+const createOrder = async () => {
+  const res = await createOrderAPI({
+    deliveryTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: "",
+    goods: checkInfo.value.goods.map((item) => {
+      return {
+        skuId: item.skuId,
+        count: item.count,
+      };
+    }),
+    addressId: curAddress.value.id,
+  });
+  const orderId = res.result.id;
+  router.push({
+    path: "/pay",
+    query: {
+      id: orderId,
+    },
+  });
+  // 更新购物车
+  cartStore.updateNewList()
 };
 </script>
 
@@ -137,7 +167,9 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large">提交订单</el-button>
+          <el-button type="primary" size="large" @click="createOrder"
+            >提交订单</el-button
+          >
         </div>
       </div>
     </div>
