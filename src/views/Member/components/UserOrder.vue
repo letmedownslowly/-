3739ad9d98c41dda4e54b1563aba0d2cc886823c
aria <2101922097@qq.com>
2023-11-19 18:39:44 +0800

    订单-分页逻辑

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -13,6 +13,7 @@ const tabTypes = [
 ];
 // 获取订单列表
 const orderList = ref([]);
+const total = ref(0);
 const params = ref({
   orderState: 0,
   page: 1,
@@ -21,14 +22,22 @@ const params = ref({
 const getOrderList = async () => {
   const res = await getUserOrder(params.value);
   orderList.value = res.result.items;
+  total.value = res.result.counts
 };
-onMounted(()=>getOrderList())
+onMounted(() => getOrderList());
 
 // tab切换
-const tabChange = (type)=>{
+const tabChange = (type) => {
   // console.log(type);
-  params.value.orderState = type
-  getOrderList()
+  params.value.orderState = type;
+  getOrderList();
+};
+
+// 页数切换
+const pageChange = (page)=>{
+  // console.log(page);
+  params.value.page = page
+  getOrderList();
 }
 </script>
 
@@ -125,7 +134,7 @@ const tabChange = (type)=>{
           </div>
           <!-- 分页 -->
           <div class="pagination-container">
-            <el-pagination background layout="prev, pager, next" />
+            <el-pagination @current-change="pageChange" :total="total" :page-size="params.pageSize" background layout="prev, pager, next" />
           </div>
         </div>
       </div>
